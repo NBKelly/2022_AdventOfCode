6486783708ac933649b14a1a0c092ab9d385b81c
NB Kelly <N.B.Kelly@protonmail.com>
2021-12-01 18:19:04 +1300

    lazy version of day 1

--- com/nbkelly/advent/Advent2021_01.java
+++ com/nbkelly/advent/Advent2021_01.java
@@ -8,6 +8,7 @@ import com.nbkelly.drafter.Timer;
 
 /* imports from file */
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Extension of Drafter directed towards a general case.
@@ -29,7 +30,31 @@ public class Advent2021_01 extends Drafter {
     @Override public int solveProblem() throws Exception {
 	Timer t = makeTimer();
 
-	println("Hello World");
+	LinkedList<Integer> window = new LinkedList<Integer>();
+	
+	int ct = 0;
+	int window_sum = 0;
+	while(hasNextLine()) {
+	    
+	    var next = nextInt();
+	    
+
+	    if(window.size() == 3) {
+		int last = window_sum;
+		window_sum -= window.poll();
+		window_sum += next;
+
+		if(window_sum > last)
+		    ct++;
+	    }
+	    		
+	    window.add(next);
+
+	    flushLine();
+	}
+
+	println(ct);
+	//println("Part one: " + ct);
 	
 	return DEBUG(1, t.split("Finished Processing"));
     }
@@ -53,8 +78,8 @@ public class Advent2021_01 extends Drafter {
 	//do whatever you want based on the commands you have given
 	//at this stage, they should all be resolved
         /* code injected from file */
-        lines = readFileLines(((FileCommand)userCommands[0]).getValue());
-        //setSource(((FileCommand)userCommands[0]).getValue());
+        //lines = readFileLines(((FileCommand)userCommands[0]).getValue());
+        setSource(((FileCommand)userCommands[0]).getValue());
 	return 0;
     }
 
