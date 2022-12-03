6ca9c824e7ad56b55283c5edef3a041980469724
NB Kelly <N.B.Kelly@protonmail.com>
2022-12-03 16:47:08 +1300

    advent 2022 day 1

---
+++ com/nbkelly/advent/Advent2022_01.java
@@ -0,0 +1,137 @@
+package com.nbkelly.advent;
+
+/* imports */
+import com.nbkelly.drafter.Drafter;
+import com.nbkelly.drafter.Command;
+import com.nbkelly.drafter.FileCommand;
+import com.nbkelly.drafter.Timer;
+
+/* imports from file */
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.Collections;
+
+/* my imported libs */
+import com.nbkelly.lib.Util;
+import com.nbkelly.drafter.BooleanCommand; //visualize cmd
+import com.nbkelly.lib.Image; //visualizer lib
+
+import com.nbkelly.lib.IntPair;
+import com.nbkelly.lib.pathfinder.Map;
+import com.nbkelly.lib.Pair;
+
+import java.math.BigInteger;
+
+
+/**
+ * Extension of Drafter directed towards a general case.
+ *
+ * @see <a href="https://nbkelly.github.io/Drafter/com/nbkelly/package-summary.html" target="_top">
+ * here</a> for the up to date online javadocs
+ */
+public class Advent2022_01 extends Drafter {
+    /* WORKFLOW:
+     *  set all needed commands with setCommands()
+     *  post-processing can be performed with actOnCommands()
+     *  the rest of your work should be based around the solveProblem() function
+     */
+
+    /* params injected from file */
+    ArrayList<String> lines;
+
+    //generate output
+    boolean generate_output = false;
+
+
+
+    /* solve problem here */
+    @Override public int solveProblem() throws Exception {
+	Timer t = makeTimer();
+
+        /* code injected from file */
+        //var ints = Util.toIntList(lines);
+
+	ArrayList<Integer> cals = new ArrayList<Integer>();
+
+	var c_cals = 0;
+	while(hasNextLine()) {
+	    var line = nextLine();
+	    if(line.length() > 0)
+		c_cals += Integer.parseInt(line);
+	    else {
+		cals.add(c_cals);
+		c_cals = 0;
+	    }
+	}
+
+	if(c_cals != 0)
+	    cals.add(c_cals);
+
+	Collections.sort(cals, Collections.reverseOrder());
+
+        DEBUGF(1, "PART ONE: "); //todo
+	println(cals.get(0));
+	//println(cals);
+        DEBUGF(1, "PART TWO: "); //todo
+	println(cals.get(0)+cals.get(1)+cals.get(2));
+
+
+        /* visualize output here */
+        generate_output();
+
+	return DEBUG(1, t.split("Finished Processing"));
+    }
+
+    /* code injected from file */
+    public void generate_output() throws Exception {
+    	if(!generate_output)
+    	    return;
+
+    	println(">generating output");
+
+    	/* output goes here */
+    }
+
+    /* set commands */
+    @Override public Command[] setCommands() {
+	//do you want paged input to be optional? This is mainly a debugging thing,
+	//or a memory management/speed thing
+	_PAGE_OPTIONAL = false; //page does not show up as a user input command
+	_PAGE_ENABLED = false;  //page is set to disabled by default
+	
+        /* code injected from file */
+        FileCommand fc = new FileCommand("Input File", "The input file for this program",
+        	       	     		 true, "--input-file", "--file");
+        
+        /* visualizer */
+        BooleanCommand vc = new BooleanCommand("Visualize Output",
+        				       "The visualized output for this program", 
+        				       false, "--out-file", "--output-file", "--out-image");
+        
+        return new Command[]{fc, vc};
+        
+        
+    }
+    
+    /* act after commands processed - userCommands stores all the commands set in setCommands */
+    @Override public int actOnCommands(Command[] userCommands) throws Exception {
+	//do whatever you want based on the commands you have given
+	//at this stage, they should all be resolved
+        /* code injected from file */
+        lines = readFileLines(((FileCommand)userCommands[0]).getValue());
+        setSource(((FileCommand)userCommands[0]).getValue());
+        
+        generate_output = ((BooleanCommand)userCommands[1]).matched();
+	return 0;
+    }
+
+    /**
+     * Creates and runs an instance of your class - do not modify
+     */
+    public static void main(String[] argv) {
+        new Advent2022_01().run(argv);
+    }
+}
